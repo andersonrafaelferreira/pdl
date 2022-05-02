211eedd569928deadf9d0d706eb1f85ade8a12c8
Rafael Ferreira <andersonrafaelferreira@gmail.com>
2022-05-03 00:01:14 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+import logo from './assets/images/iguatemi.png'
+
+function App() {
+  const [username, setUsername] = useState('')
+
+  return (
+    <main>
+    <div className="box">
+    <header>
+          <img src={logo} alt="" className='logo'/>
+        </header>
+      <div className="loginBox">
+        
+
+        <form action="">
+        <h1 className='title'>Bem-vindo</h1>
+        <p className='description'>Portal do Lojista, para acessar insira seus dados.</p>
+        <label htmlFor="">
+        Usuário
+        </label>
+        <input type="text"  id="username" value="" placeholder='Digite seu usuário' autoComplete='false' />
+        <label htmlFor="">
+        Senha
+        </label>
+        <input type="password"  id="password" value="rafael" placeholder='Digite sua senha' autoComplete='false' />
+        <small className='warn'>Esqueceu sua senha? <a href='#'>clique aqui</a>
+  </small>
+        <button>Entrar</button>
+        </form>
+      </div>
+      <footer>© Portal do Lojista 2022</footer>
+    </div>
+    </main>
+  )
+}
+
+export default App
